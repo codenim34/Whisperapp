cdb871da50879a8944cf50bfdc954077e50e7d54
ash_tasnim_1834 <tasnim.iterator.34@gmail.com>
2023-07-25 23:16:49 +0600

    commited after encrypting

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -13,14 +15,10 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"] });
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -36,48 +34,42 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
-
-
-
-app.post("/register",function (req,res){
-      const newUser = new User ( {
-           email: req.body.username,
-           password : req.body.password,
-           
-      }) ;
-      newUser.save().then(function(){
-         res.render("secrets");
-      }).catch(function(err){
+app.post("/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds).then(function (hash) {
+    // Store hash in your password DB.
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser
+      .save()
+      .then(function () {
+        res.render("secrets");
+      })
+      .catch(function (err) {
         console.log(err);
       });
-      
-})
+  });
+});
 
-app.post("/login",function(req,res){
-     let username = req.body.username;
-     let password = req.body.password;
+app.post("/login", function (req, res) {
+  let username = req.body.username;
+  let password = req.body.password;
 
-     User.findOne({email: username}).then(function(foundUser){
-         if(foundUser.password === password){
-            res.render("secrets");
+  User.findOne({ email: username })
+    .then(function (foundUser) {
+      bcrypt.compare(password, foundUser.password).then(function(result) {
+         if(result===true) {
+           res.render("secrets");
          }else{
-            console.log("password doesn't match");
+          console.log("Wrong  pass");
          }
-     }).catch(function(err){
-        console.log(err);
-     });
-})
-
-
-
-
-
-
-
-
-
-
-
+    });
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+});
 
 app.listen(3000, function () {
   console.log("Server started on 3000 portal");
